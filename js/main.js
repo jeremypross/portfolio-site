4ef3b9b088674f203202d87deb116d1b9835d83e
Jeremy Ross <jeremypross@gmail.com>
2017-04-16 18:16:31 -0400

    changed graphics, styling

--- js/main.js
+++ js/main.js
@@ -20,30 +20,30 @@ $(document).ready(() => {
   // setTimeout for h1
   setTimeout(() => {
     $head.css('visibility', 'visible');
-    $head.addClass('animated fadeIn');
+    $head.addClass('animated fadeInDownBig');
   }, 400);
 
   // setTimeout for h2
   setTimeout(() => {
     $webDeveloper.css('visibility', 'visible');
     $webDeveloper.addClass('animated fadeIn');
-  }, 1000);
+  }, 1600);
 
   // setTimeout for social media icons
   setTimeout(() => {
     $gitHub.css('visibility', 'visible');
-    $gitHub.addClass('animated fadeInUp');
+    $gitHub.addClass('animated fadeIn');
     $linkedIn.css('visibility', 'visible');
-    $linkedIn.addClass('animated fadeInUp');
+    $linkedIn.addClass('animated fadeIn');
     $instagram.css('visibility', 'visible');
-    $instagram.addClass('animated fadeInUp');
-  }, 1400);
+    $instagram.addClass('animated fadeIn');
+  }, 2600);
 
   // setTimeout for about
   setTimeout(() => {
     $scroll.css('visibility', 'visible');
-    $scroll.addClass('animated bounce');
-  }, 1800);
+    $scroll.addClass('animated rubberBand');
+  }, 3200);
 
 
 });
